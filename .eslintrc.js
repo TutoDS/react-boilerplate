0f357b103d8b6a12b7dd9736f3f232bdddf41959
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-04-12 14:11:58 +0100

    Try new eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,53 @@
 module.exports = {
+	root: true,
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2020,
 		sourceType: 'module',
 		ecmaFeatures: {
-			jsx: true // Allows for the parsing of JSX
+			jsx: true
 		}
 	},
 	settings: {
-		'import/parsers': {
-			'@typescript-eslint/parser': ['.ts', '.tsx']
-		},
-		'import/resolver': {
-			typescript: {
-				alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-
-				// Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
-
-				// use <root>/path/to/folder/tsconfig.json
-				project: './'
-			}
-		},
-		node: {},
 		react: {
 			version: 'detect'
+		},
+		'import/resolver': {
+			node: {
+				paths: ['src'],
+				extensions: ['.js', '.jsx', '.ts', '.tsx']
+			}
 		}
 	},
-	plugins: ['etc', 'import'],
+	env: {
+		browser: true,
+		amd: true,
+		node: true
+	},
 	extends: [
-		'plugin:import/errors',
-		'plugin:import/warnings',
-		'plugin:import/typescript',
+		'eslint:recommended',
 		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:import/errors',
-		'plugin:import/warnings',
-		'plugin:import/typescript',
 		'plugin:jsx-a11y/recommended',
-		'plugin:prettier/recommended' //should be at the last
+		'plugin:prettier/recommended' // Make sure this is always the last element in the array.
 	],
+	plugins: ['simple-import-sort', '@typescript-eslint', 'prettier'],
 	rules: {
-		'etc/no-implicit-any-catch': ['error', { allowExplicitAny: true }],
 		'no-unused-vars': 'off',
 		'@typescript-eslint/no-unused-vars': ['error'],
-		'@typescript-eslint/no-var-requires': 'off',
-		'react-hooks/rules-of-hooks': 'error',
-		'react-hooks/exhaustive-deps': 'warn',
-		'react/prop-types': 'off',
-		'react/jsx-uses-react': 'off',
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }],
 		'react/react-in-jsx-scope': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-implicit-any-catch': ['error', { allowExplicitAny: true }]
+		'jsx-a11y/accessible-emoji': 'off',
+		'react/prop-types': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+		'jsx-a11y/anchor-is-valid': [
+			'error',
+			{
+				components: ['Link'],
+				specialLink: ['hrefLeft', 'hrefRight'],
+				aspects: ['invalidHref', 'preferButton']
+			}
+		]
 	}
 };
